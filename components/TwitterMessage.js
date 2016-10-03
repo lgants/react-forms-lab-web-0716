5815c5b9c15ced970186f4afcf52aceaa99c456d
lgants <lgants@gmail.com>
2016-10-02 20:15:48 -0400

    react forms lab complete

--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -4,17 +4,38 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: '',
+    };
+
+    this.setMessage = this.setMessage.bind(this);
+  }
+
+  setMessage(event){
+    this.setState({
+      message: event.target.value,
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.message} onChange={this.setMessage}/>
+        <span>{this.props.maxChars - this.state.message.length}</span>
       </div>
     );
   }
 }
+
+TwitterMessage.propTypes = {
+  maxChars: React.PropTypes.number,
+};
+
+TwitterMessage.defaultprops = {
+  maxChars: 140,
+};
+
+
 
 module.exports = TwitterMessage;
